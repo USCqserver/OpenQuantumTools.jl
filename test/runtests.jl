ead21a160085cca35d746caae6108dfe54d37625
neversakura <neversakura@hotmail.com>
2018-12-16 14:48:36 -0800

    add unitary_check

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,4 +24,6 @@ end
     sol = calculate_unitary(hfun)
     u_res = exp(-1.0im*5*0.5*σx)
     @test isapprox(u_res, sol(0.5), rtol=1e-6, atol=1e-8)
+    @test unitary_check(u_res)
+    @test !unitary_check([0 1; 0 0])
 end
