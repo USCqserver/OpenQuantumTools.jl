738806a8b6e51bc7892a32eeb1d91a10b515749b
neversakura <neversakura@hotmail.com>
2019-04-19 11:19:25 -0700

    Bug fix: expose HybridOhmic function

--- src/Bath/Bath.jl
+++ src/Bath/Bath.jl
@@ -1,7 +1,7 @@
 module Bath
 
 export OhmicBath, Ohmic, γ, S, correlation, polaron_correlation, interpolate_spectral_density
-export HybridOhmicBath
+export HybridOhmicBath, HybridOhmic
 
 include("../QTBase/QTBase.jl")
 import .QTBase:beta_2_temperature, temperature_2_beta, freq_2_temperature, temperature_2_freq, cauchy_principal_value, construct_interpolations
