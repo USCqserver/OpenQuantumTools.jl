1ec23cb3c7ba538b58948180d1d23188d3fd6b6d
neversakura <neversakura@hotmail.com>
2019-04-26 11:41:13 -0700

    Add rotate_by_interaction

--- src/QTBase/proj_util.jl
+++ src/QTBase/proj_util.jl
@@ -7,6 +7,16 @@ struct LowLevelParams
     lvl::Int
 end
 
+struct RotatedTwoLevelParams
+    s::AbstractArray{Float64, 1}
+    ω::Array{Float64, 1}
+    T::Array{ComplexF64, 1}
+    a::Array{Float64, 1}
+    b::Array{Float64, 1}
+    c::Array{Float64, 1}
+    d::Array{Float64, 1}
+end
+
 function get_dθ(params::LowLevelParams, i, j)
     if j > i
         idx = (2 * params.lvl - i ) * (i - 1) ÷ 2 + (j - i)
@@ -140,4 +150,38 @@ function optimal_interaction_rotation(low::LowLevelParams)
         push!(opt_θ, opt.minimizer)
     end
     opt_θ
+end
+
+function rotate_by_interaction(params::LowLevelParams, θ)
+    g = [1.0, 0]
+    e = [0, 1.0]
+    ω = []
+    T = []
+    a = []
+    b = []
+    c = []
+    d = []
+    for i in eachindex(params.s)
+        H = [params.ev[i][1] -1.0im*params.dθ[i][1]; 1.0im*params.dθ[i][1] params.ev[i][2]]
+        U = [-cos(θ[i]/2 - π/4) -cos(θ[i]/2 + π/4); cos(θ[i]/2 + π/4) -cos(θ[i]/2 - π/4)]
+        Hr = U * H * U'
+        push!(ω, real(Hr[1,1]-Hr[2,2]))
+        push!(T, Hr[1,2])
+        at = 0.0
+        bt = 0.0
+        ct = 0.0
+        dt = 0.0
+        for op in params.op[i]
+            or = U * op * U'
+            at += (or[1,1] - or[2,2])^2
+            bt += abs2(or[1,2])
+            ct += or[1,2] * (or[1,1] - or[2,2])
+            dt +=  or[1,2] * (or[1,1] + or[2,2])
+        end
+        push!(a, at)
+        push!(b, bt)
+        push!(c, ct)
+        push!(d, dt)
+    end
+    RotatedTwoLevelParams(params.s, ω, T, a, b, c, d)
 end
