37c8f9a6b51183e69d07814b95b0e48e0109a769
neversakura <neversakura@hotmail.com>
2019-04-26 14:53:29 -0700

    Bug fix
    Export optimal_interaction_rotation function

--- src/QTBase/QTBase.jl
+++ src/QTBase/QTBase.jl
@@ -9,13 +9,15 @@ import Optim:optimize
 
 export temperature_2_beta, temperature_2_freq, beta_2_temperature, freq_2_temperature
 
-export σx, σz, σy, σi, σ, ⊗, PauliVec, spσx, spσz, spσi, spσy, LowLevelParams, RotatedTwoLevelParams
+export σx, σz, σy, σi, σ, ⊗, PauliVec, spσx, spσz, spσi, spσy
 
 export q_translate, construct_hamming_weight_op, ising_terms, standard_driver, collective_operator, GHZ_entanglement_witness, local_field_term, two_local_term
 
 export matrix_decompose, check_positivity
 
-export inst_population, gibbs_state, eigen_eval, eigen_state_continuation!, low_level_hamiltonian, minimum_gap, proj_low_lvl, optimal_interaction_rotation, get_dθ
+export inst_population, gibbs_state, eigen_eval, eigen_state_continuation!, low_level_hamiltonian, minimum_gap
+
+export  LowLevelParams, RotatedTwoLevelParams, rotate_by_interaction, proj_low_lvl, optimal_interaction_rotation, get_dθ
 
 include("unit_util.jl")
 include("math_util.jl")
