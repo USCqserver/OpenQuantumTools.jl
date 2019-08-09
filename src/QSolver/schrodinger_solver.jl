30b2e709dd852f9fedd3da3b59d705a671a9a0ff
neversakura <neversakura@hotmail.com>
2019-08-09 14:11:55 -0700

    Update parallel schrodinger in physical unit

--- src/QSolver/schrodinger_solver.jl
+++ src/QSolver/schrodinger_solver.jl
@@ -6,16 +6,18 @@ function solve_schrodinger(A::Annealing, tf::Real; kwargs...)
     p = AnnealingParams(A.H, float(tf))
     jp = zeros(eltype(u0), length(u0), length(u0))
     if haskey(kwargs, :span_unit) && kwargs[:span_unit] == true
-        t_span = (tf * A.sspan[1], tf * A.sspan[2])
+        tspan = (tf * A.sspan[1], tf * A.sspan[2])
+        tstops = tf * A.tstops
         ff = ODEFunction(sch_f_unit; jac = sch_jac_unit, jac_prototype = jp)
     else
-        t_span = A.sspan
+        tspan = A.sspan
+        tstops = A.tstops
         ff = ODEFunction(
             sch_f_unitless;
             jac = sch_jac_unitless, jac_prototype = jp
         )
     end
-    prob = ODEProblem{true}(ff, u0, t_span, p)
+    prob = ODEProblem{true}(ff, u0, tspan, p)
     solve(prob; alg_hints = [:nonstiff], tstops = A.tstops, kwargs...)
 end
 
@@ -30,27 +32,47 @@ function solve_schrodinger(
         throw(DomainError("Initial state must be given as a state vector."))
     end
     u0 = sch_prepare_u0(A.u0)
-    p = AnnealingParams(A.H, float(tf[1]))
+    p = AnnealingParams(A.H, 1.0)
     jp = zeros(eltype(u0), length(u0), length(u0))
     # trajectories numbers
     trajectories = length(tf)
     tf_arr = float.(tf)
     if haskey(kwargs, :span_unit) && kwargs[:span_unit] == true
-        t_span = (float(tf[1]) * A.sspan[1], float(tf[1]) * A.sspan[2])
+        if !isempty(A.tstops)
+            @warn "Parallel algorithm does not support scaling tstops argument. Setting it to be empty."
+            tstops = []
+        end
         ff = ODEFunction(sch_f_unit; jac = sch_jac_unit, jac_prototype = jp)
+        prob_func = (
+            prob,
+            i,
+            repeat
+        ) -> begin
+            prob.p.H = p_copy(prob.p.H)
+            tspan = (
+                prob.tspan[1] * tf_arr[i],
+                prob.tspan[2] * tf_arr[i]
+            )
+            prob.p.tf = tf_arr[i]
+            ODEProblem{true}(prob.f, prob.u0, tspan, prob.p)
+        end
     else
-        t_span = A.sspan
+        tstops = A.tstops
         ff = ODEFunction(
             sch_f_unitless;
             jac = sch_jac_unitless, jac_prototype = jp
         )
+        prob_func = (
+            prob,
+            i,
+            repeat
+        ) -> begin
+            prob.p.H = p_copy(prob.p.H)
+            prob.p.tf = tf_arr[i]
+            prob
+        end
     end
-    function prob_func(prob, i, repeat)
-        prob.p.H = p_copy(prob.p.H)
-        prob.p.tf = tf_arr[i]
-        prob
-    end
-    prob = ODEProblem{true}(ff, u0, t_span, p)
+    prob = ODEProblem{true}(ff, u0, A.sspan, p)
     ensemble_prob = EnsembleProblem(prob; prob_func = prob_func)
     solve(
         ensemble_prob,
@@ -59,6 +81,8 @@ function solve_schrodinger(
         trajectories = trajectories, tstops = A.tstops, kwargs...
     )
 end
+
+function ode_with_unit() end
 
 function sch_f_unitless(du, u, p, t)
     p.H(du, u, p.tf, t)
