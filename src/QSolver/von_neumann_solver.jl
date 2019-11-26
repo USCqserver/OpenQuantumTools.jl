ee8c6f6d93448cc8b584ada6315838ba310f8921
Huo Chen <neversakura@hotmail.com>
2019-11-26 00:22:05 -0800

    Add vectorize option to VonNeumann/Redfield solver (#33)
    * Add vectorize option to Redfield solver
    
    * Fix reltol in redfield solver test

--- src/QSolver/von_neumann_solver.jl
+++ src/QSolver/von_neumann_solver.jl
@@ -1,22 +1,43 @@
-function solve_von_neumann(A::Annealing, tf::Real; span_unit = false, kwargs...)
-    tstops = A.tstops
-    u0 = build_u0(A.u0, :m, control=A.control)
+function solve_von_neumann(
+    A::Annealing,
+    tf::Real;
+    span_unit = false,
+    vectorize = false,
+    tstops = Float64[],
+    kwargs...,
+)
     tf = build_tf(tf, span_unit)
-    if typeof(A.control) <: PausingControl
-        ff = ODEFunction{true}(
-            von_control_f;
-            jac = von_control_jac
-        )
-        cb = DiscreteCallback(pause_condition, pause_affect!)
-        kwargs = Dict{Symbol, Any}(kwargs)
-        kwargs[:callback] = cb
-    else
-        ff = ODEFunction{true}(von_f; jac = von_jac)
-    end
-    tspan, tstops = scaling_time(tf, A.sspan, tstops)
+    tstops = build_tstops(tf, tstops, A.tstops)
+    u0 = build_u0(A.u0, :m, control = A.control, vectorize = vectorize)
+    callback = build_callback(A.control, :von_neumann)
+    ff = von_neumann_construct_ode_function(A.H, A.control, vectorize)
     p = AnnealingParams(A.H, tf; control = A.control)
-    prob = ODEProblem{true}(ff, u0, tspan, p)
-    solve(prob; alg_hints = [:nonstiff], tstops = tstops, kwargs...)
+    prob = ODEProblem{true}(ff, u0, (p) -> scaling_tspan(p.tf, A.sspan), p)
+    solve(prob; alg_hints = [:nonstiff], tstops = tstops, callback = callback, kwargs...)
+end
+
+
+function von_neumann_construct_ode_function(
+    H,
+    control::Union{Nothing,InstPulseControl},
+    vectorize::Bool,
+)
+    cache = get_cache(H, vectorize)
+    if vectorize == false
+        ff = ODEFunction{true}(von_f; jac = von_jac)
+    else
+        diff_op = DiffEqArrayOperator(
+            cache,
+            update_func = (A, u, p, t) -> update_vectorized_cache!(A, p.H, p.tf, t),
+        )
+        jac_cache = similar(cache)
+        jac_op = DiffEqArrayOperator(
+            jac_cache,
+            update_func = (A, u, p, t) -> update_vectorized_cache!(A, p.H, p.tf, t),
+        )
+        ff = ODEFunction(diff_op; jac_prototype = jac_op)
+    end
+    ff
 end
 
 
@@ -28,7 +49,7 @@ end
 function von_jac(J, u, p, t)
     hmat = p.H(p.tf, t)
     iden = Matrix{eltype(hmat)}(I, p.H.size)
-    temp = (iden⊗hmat - transpose(hmat)⊗iden)
+    temp = (iden ⊗ hmat - transpose(hmat) ⊗ iden)
     mul!(J, -1.0im, temp)
 end
 
@@ -39,6 +60,6 @@ end
 function von_control_jac(J, u, p, t)
     hmat = p.H(u, p, t)
     iden = Matrix{eltype(hmat)}(I, p.H.size)
-    temp = (iden⊗hmat - transpose(hmat)⊗iden)
+    temp = (iden ⊗ hmat - transpose(hmat) ⊗ iden)
     mul!(J, -1.0im, temp)
 end
--- test/QSolvers/redfield_solver_test.jl
+++ test/QSolvers/redfield_solver_test.jl
@@ -9,8 +9,8 @@ coupling = ConstantCouplings(["Z"], unit = :ħ)
 bath = Ohmic(1e-4, 4, 16)
 annealing = Annealing(H, u0; coupling = coupling, bath = bath)
 tf = 10
-U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, retol = 1e-8);
-sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, retol = 1e-8);
+U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, reltol = 1e-8);
+sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, reltol = 1e-8);
 cfun(t) = correlation(t, bath)
 f(t) = QuantumAnnealingTools.quadgk(cfun, 0, t)[1]
 γ = real(QuantumAnnealingTools.quadgk(f, 0, tf)[1])
@@ -24,7 +24,7 @@ sol = solve_redfield(
     U;
     alg = Tsit5(),
     abstol = 1e-8,
-    retol = 1e-8,
+    reltol = 1e-8,
     positivity_check = true,
 );
 @test !(1.0 in sol.t)
@@ -37,12 +37,12 @@ tf = 20
 cfun(x) = x <= 20 ? 1e-4 : 0
 bath = CustomBath(correlation = cfun)
 annealing = Annealing(H, u0; coupling = coupling, bath = bath)
-U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, retol = 1e-8);
-sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, retol = 1e-8);
+U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, reltol = 1e-8);
+sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, reltol = 1e-8);
 @test sol(1.0)[1, 2] ≈ exp(-4 * 0.02) * 0.5 atol = 1e-4 rtol = 1e-4
 
 control = InstPulseControl([0.5], (x) -> σx)
 annealing = Annealing(H, u0, control = control, coupling = coupling, bath = bath)
-U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, retol = 1e-8);
-sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, retol = 1e-8, tstops = [0.51]);
+U = solve_unitary(annealing, tf, alg = Tsit5(), abstol = 1e-8, reltol = 1e-8);
+sol = solve_redfield(annealing, tf, U; alg = Tsit5(), abstol = 1e-8, reltol = 1e-8, tstops = [0.51]);
 @test sol(1.0)[1, 2] ≈ 0.5 atol = 1e-3
