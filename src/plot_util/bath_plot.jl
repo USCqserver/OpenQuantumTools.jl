ccc5f308ce01184c25c9369e51175f6fc703333d
Huo Chen <neversakura@hotmail.com>
2020-06-05 15:13:36 -0700

    Change bath/Hamiltonian plot recipe

--- src/plot_util/bath_plot.jl
+++ src/plot_util/bath_plot.jl
@@ -1,13 +1,13 @@
 @recipe function f(bath::OhmicBath, func_symbol, ω)
     if (func_symbol == :γ) || (func_symbol == :spectrum)
-        ylabel --> L"\frac{\gamma(\omega)}{\eta g^2 \omega_c}"
+        yguide --> L"\frac{\gamma(\omega)}{\eta g^2 \omega_c}"
     elseif func_symbol == :S
-        ylabel --> L"\frac{S(\omega)}{\eta g^2 \omega_c}"
+        yguide --> L"\frac{S(\omega)}{\eta g^2 \omega_c}"
     else
         throw(ArgumentError("Must be :γ or :S for Ohmic bath."))
     end
     y = [eval(Expr(:call, func_symbol, x, bath)) for x in ω * bath.ωc]
-    xlabel --> L"\omega / \omega_c"
+    xguide --> L"\omega / \omega_c"
     (ω, y ./ bath.η / bath.ωc)
 end
 
