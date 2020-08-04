128fb304127ed96ada58d755b69f3934453b89bb
Huo Chen <neversakura@hotmail.com>
2020-08-04 14:44:35 -0700

    fix test failure of StochasticSchrodinger equation

--- test/QSolvers/stochastic_schrodinger.jl
+++ test/QSolvers/stochastic_schrodinger.jl
@@ -1,37 +1,32 @@
 using QuantumAnnealingTools, Test, Random
 using OrdinaryDiffEq
 
-H = DenseHamiltonian([(s) -> 1.0], -[σz], unit = :ħ)
+H = DenseHamiltonian([(s) -> 1.0], -[σz], unit=:ħ)
 u0 = PauliVec[1][1]
-coupling = ConstantCouplings(["Z"], unit = :ħ)
+coupling = ConstantCouplings(["Z"], unit=:ħ)
 bath = EnsembleFluctuator([1.0, 2.0], [2.0, 1.0])
-annealing = Annealing(H, u0, coupling = coupling, bath = bath)
+annealing = Annealing(H, u0, coupling=coupling, bath=bath)
 tf = 2.0
 
-prob = build_ensembles(annealing,tf,:stochastic)
+prob = build_ensembles(annealing, tf, :stochastic)
 Random.seed!(1234)
-sol = solve(prob, Tsit5(), EnsembleSerial(), trajectories=1)
-tstops = []
-for i = 2:length(sol[1].t)
-    if sol[1].t[i] == sol[1].t[i-1]
-        push!(tstops, sol[1].t[i])
+sol1 = solve(prob, Tsit5(), EnsembleSerial(), trajectories=1)
+tstops1 = []
+for i = 2:length(sol1[1].t)
+    if sol1[1].t[i] == sol1[1].t[i - 1]
+        push!(tstops1, sol1[1].t[i])
     end
 end
-exp_res = [
-    0.6595793462134859 + 0.2607910764094174im
-    0.6595793462134859 - 0.2607910764094174im
-]
-exp_jump_t = [
-    0.30221824355127086
-    0.9554780593261047
-    1.2999133449599989
-    1.5091713624528147
-    1.5198805867595513
-    1.5666432314629304
-    1.7148763674267669
-    1.8740639245513286
-    1.9423859799119376
-]
 
-@test sol[1][end] == exp_res
-@test tstops == exp_jump_t
+Random.seed!(1234)
+sol2 = solve(prob, Tsit5(), EnsembleSerial(), trajectories=1)
+tstops2 = []
+for i = 2:length(sol2[1].t)
+    if sol2[1].t[i] == sol2[1].t[i - 1]
+        push!(tstops2, sol2[1].t[i])
+    end
+end
+
+@test sol1[1][end] ≈ sol2[1][end] atol = 1e-6 rtol = 1e-6
+@test !isempty(tstops1)
+@test tstops1 == tstops2
