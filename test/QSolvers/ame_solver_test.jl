9938e4eac0f2ac5083d725035c792ae65c6fceb7
Huo Chen <neversakura@hotmail.com>
2021-01-20 22:26:32 -0800

    ✅ added save_positions test for build_ensemble :ame interface

--- test/QSolvers/ame_solver_test.jl
+++ test/QSolvers/ame_solver_test.jl
@@ -1,4 +1,4 @@
-using OpenQuantumTools, Test, OrdinaryDiffEq
+using OpenQuantumTools, Random, Test, OrdinaryDiffEq
 
 
 H = DenseHamiltonian([(s) -> 1.0], [σi], unit = :ħ)
@@ -32,3 +32,15 @@ sol = solve_ame(
     reltol = 1e-8,
 )
 @test sol(tf)[1, 2] ≈ exp(-2 * γ * tf) * 0.5 atol = 1e-5 rtol = 1e-5
+
+tf = 1000
+prob = build_ensembles(annealing, tf, :ame, save_positions=(true, true))
+Random.seed!(1234)
+sol1 = solve(prob, Tsit5(), EnsembleSerial(), trajectories=1, save_everystep=false)[1]
+
+prob = build_ensembles(annealing, tf, :ame)
+Random.seed!(1234)
+sol2 = solve(prob, Tsit5(), EnsembleSerial(), trajectories=1, save_everystep=false)[1]
+
+@test !(length(sol1) == 2)
+@test sol1[end] ≈ sol2[end]
